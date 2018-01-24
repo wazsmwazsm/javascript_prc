7771efe9856b26a7977fa6ace74e495ac5c26d8b
wazsmwazsm <18748406022@163.com>
2018-01-24 17:17:32 +0800

    webpack sample add react, babel loader

--- webpack/sample/app/Greeter.js
+++ webpack/sample/app/Greeter.js
@@ -1,6 +1,24 @@
-module.exports = function() {
-    var greet = document.createElement('div');
-    greet.textContent = 'Hi there and greetings!';
+// var config = require('./config.json');
 
-    return greet;
-};
+// module.exports = function() {
+//     var greet = document.createElement('div');
+//     greet.textContent = config.greetText;
+//
+//     return greet;
+// };
+
+// 使用 ECS 6 语法
+import React, {Component} from 'react';
+import config from './config.json';
+
+class Greeter extends Component {
+    render () {
+        return (
+            <div>
+              {config.greetText + 'react'}
+            </div>
+        );
+    }
+}
+
+export default Greeter;
--- webpack/sample/app/main.js
+++ webpack/sample/app/main.js
@@ -1,6 +1,13 @@
-const greeter = require('./Greeter.js');
-
-document.querySelector("#root").appendChild(greeter());
-
 // 手动 webpack 打包
 // webpack {entry file} {destination for bundled file}
+
+// const greeter = require('./Greeter.js');
+//
+// document.querySelector("#root").appendChild(greeter());
+
+// 使用 react 和 ES6 语法
+import React from 'react';
+import {render} from 'react-dom';
+import Greeter from './Greeter';
+
+render(<Greeter />, document.getElementById('root'));
